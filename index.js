4a045da6374836bdf1c1a68061c8d92137052ef8
Ramya-Geethika <geethikakramya@gmail.com>
2020-08-28 13:37:57 +0000

    LightCoin

--- index.js
+++ index.js
@@ -1,29 +1,81 @@
-let balance = 500.00;
+class Account {
 
-class Withdrawal {
+  constructor() {
+    this.transactions = [];
+  }
 
-  constructor(amount) {
-    this.amount = amount;
+  get balance() {
+  	let balance = 0;
+    for (let i of this.transactions) {
+    	balance += i.value;
+    }
+    return balance;
+  }
+
+  addTransaction(transaction) {
+  	this.transactions.push(transaction);
+  }
+}
+
+// abstract class
+class Transaction {
+
+  constructor(amount, account) {
+    this.amount  = amount;
+    this.account = account;
   }
 
   commit() {
-    balance -= this.amount;
+    if (!this.isAllowed()) {
+      return false;
+    }
+    this.time = new Date();
+    this.account.addTransaction(this);
+    return true;
   }
-
 }
 
+class Withdrawal extends Transaction {
 
+  get value() {
+    return -this.amount;
+  }
 
+  isAllowed() {
+    return (this.account.balance - this.amount >= 0);
+  }
+}
 
-// DRIVER CODE BELOW
-// We use the code below to "drive" the application logic above and make sure it's working as expected
+class Deposit extends Transaction {
 
-t1 = new Withdrawal(50.25);
-t1.commit();
-console.log('Transaction 1:', t1);
+  get value() {
+    return this.amount;
+  }
 
-t2 = new Withdrawal(9.99);
-t2.commit();
-console.log('Transaction 2:', t2);
+  isAllowed() {
+    return true;
+  }
+}
 
-console.log('Balance:', balance);
+const myAccount = new Account();
+
+console.log('Starting Account Balance: ', myAccount.balance);
+
+console.log('Attempting to withdraw even $1 should fail...');
+const t1 = new Withdrawal(1.00, myAccount);
+console.log('Commit result:', t1.commit());
+console.log('Account Balance: ', myAccount.balance);
+
+console.log('Depositing should succeed...');
+const t2 = new Deposit(20.45, myAccount);
+console.log('Commit result:', t2.commit());
+console.log('Account Balance: ', myAccount.balance);
+
+console.log('Withdrawal for 9.99 should be allowed...');
+const t3 = new Withdrawal(20.45, myAccount);
+console.log('Commit result:', t3.commit());
+
+console.log('Ending Account Balance: ', myAccount.balance);
+console.log("Lookings like I'm broke again");
+
+console.log('Account Transaction History: ', myAccount.transactions);
